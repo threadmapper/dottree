7b5e09dc51f96a37dae4e7a327eb340a017db9f0
gitbackspacer <49907563+gitbackspacer@users.noreply.github.com>
2020-02-28 14:06:11 +0000

    Update __init__.py

--- dottree/__init__.py
+++ dottree/__init__.py
@@ -1,7 +1,22 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from collections import defaultdict
 
+
+# a dict based solution 
+class Viv(dict):
+
+    def __missing__(self, key):
+       val = self[key] = type(self)() 
+       return val  
+
+    def __getattr__(self, key):
+        return self[key]
+
+    def __setattr__(self, key, val):
+        self[key] = val
+
+# a def_dict base 
 class viv_dotdict(defaultdict):
 
     def __getattr__(self, key):
@@ -11,4 +26,10 @@ class viv_dotdict(defaultdict):
         self[key] = val
 
 def dotree(): return viv_dotdict(dotree)
+
+
+
+ 
+ 
+
 
